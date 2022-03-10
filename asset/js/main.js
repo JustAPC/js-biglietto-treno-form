c3bf42ded98b64da0e1165de7d93e65c0f99f6e7
Andrea Pontillo <andreapontillo97@gmail.com>
2022-03-10 17:01:25 +0100

    modifiche funzionamento js

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,34 +1,33 @@
 let inputNome = document.getElementById("inputnome");
 let inputKm = document.getElementById("inputkm");
-let eta = document.getElementById("età")
+let eta = document.getElementById("eta")
 
 let bottoneGenera = document.getElementById("bottone-genera");
 let resoconto = document.getElementById("resoconto");
 
-let prezzoBase = (inputKm.value * 0.21);
-let prezzoMaggiorenni = prezzoBase - (prezzoBase * 0.2);
-let prezzoOver65 = prezzoBase - (prezzoBase * 0.4);
-
 bottoneGenera.addEventListener("click",
-  function(){
+function(){
     console.log(inputNome.value);
     console.log(inputKm.value);
     console.log(eta.value);
+    let prezzoBase = (inputKm.value * 0.21);
 
     if (eta.value == 1){
-        document.getElementById("biglietto-scontato").innerHTML = "Biglietto Scontato" 
-        document.getElementById("prezzo-finale").innerHTML = prezzoMaggiorenni.toFixed(2) + "&#8364;" 
+        let prezzoMinorenni = prezzoBase - (prezzoBase * 0.2);
+        document.getElementById("categoria-biglietto").innerHTML = "Biglietto Scontato" 
+        document.getElementById("prezzo-finale").innerHTML = prezzoMinorenni.toFixed(2) + "&#8364;" 
     } else if (eta.value == 2) {
-        document.getElementById("biglietto-standard").innerHTML = "Biglietto Standard" 
+        document.getElementById("categoria-biglietto").innerHTML = "Biglietto Standard" 
         document.getElementById("prezzo-finale").innerHTML = prezzoBase.toFixed(2) + "&#8364;" 
     } else if (eta.value == 3) {
-        document.getElementById("biglietto-scontato").innerHTML = "Biglietto Scontato" 
+        let prezzoOver65 = prezzoBase - (prezzoBase * 0.4);
+        document.getElementById("categoria-biglietto").innerHTML = "Biglietto Scontato" 
         document.getElementById("prezzo-finale").innerHTML = prezzoOver65.toFixed(2) + "&#8364;" 
     }
+    document.getElementById("nome").innerHTML = inputNome.value
+    document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10) + 1;
+    document.getElementById("codice-cp").innerHTML = Math.floor(Math.random() * 99999) + 9999;
   }
 );
 
-document.getElementById("nome").innerHTML = inputNome.value
-document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10) + 1;
-document.getElementById("codice-cp").innerHTML = Math.floor(Math.random() * 99999) + 9999;
 
