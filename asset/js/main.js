59cc5b672c40bac4097a096abba3d60e7d060f49
Andrea Pontillo <andreapontillo97@gmail.com>
2022-03-10 16:00:40 +0100

    ulteriori modifiche

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,37 +1,34 @@
 let inputNome = document.getElementById("inputnome");
 let inputKm = document.getElementById("inputkm");
-let età = document.getElementById("età")
+let eta = document.getElementById("età")
 
 let bottoneGenera = document.getElementById("bottone-genera");
 let resoconto = document.getElementById("resoconto");
 
-let prezzoScontato = 0;
+let prezzoBase = (inputKm.value * 0.21);
+let prezzoMaggiorenni = prezzoBase - (prezzoBase * 0.2);
+let prezzoOver65 = prezzoBase - (prezzoBase * 0.4);
 
 bottoneGenera.addEventListener("click",
   function(){
     console.log(inputNome.value);
     console.log(inputKm.value);
-    console.log(età.value);
+    console.log(eta.value);
 
-
-    let prezzoBase = (inputKm.value * 0.21);
-    console.log("Il prezzo base del biglietto è di " + prezzoBase + "€");
-
-    if (età.value = 1){
-        prezzoScontato = prezzoBase - (prezzoBase * 0.2);
+    if (eta.value = 1){
         document.getElementById("biglietto-scontato").innerHTML = "Biglietto Scontato" 
-        document.getElementById("prezzo-finale").innerHTML = prezzoScontato.toFixed(2) + "&#8364;" 
-    } else if (età.value = 2) {
+        document.getElementById("prezzo-finale").innerHTML = prezzoMaggiorenni.toFixed(2) + "&#8364;" 
+    } else if (eta.value = 2) {
         document.getElementById("biglietto-standard").innerHTML = "Biglietto Standard" 
         document.getElementById("prezzo-finale").innerHTML = prezzoBase.toFixed(2) + "&#8364;" 
-    } else if (età.value = 3) {
-        prezzoScontato = prezzoBase - (prezzoBase * 0.4);
+    } else if (eta.value = 3) {
         document.getElementById("biglietto-scontato").innerHTML = "Biglietto Scontato" 
-        document.getElementById("prezzo-finale").innerHTML = prezzoScontato.toFixed(2) + "&#8364;" 
+        document.getElementById("prezzo-finale").innerHTML = prezzoOver65.toFixed(2) + "&#8364;" 
     }
   }
 );
 
+document.getElementById("nome").innerHTML = inputNome.value
 document.getElementById("carrozza").innerHTML = Math.floor(Math.random() * 10) + 1;
 document.getElementById("codice-cp").innerHTML = Math.floor(Math.random() * 99999) + 9999;
 
